1a327f8e8d7b18f8c577042526761608f945fd0c
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-12-27 20:09:44 +0700

    feat(boss logic): trying to create a logic for the blind / boss and stuff

--- src/utils/blind.js
+++ src/utils/blind.js
@@ -1,10 +1,16 @@
-// TODO: make object for each ante as their key and the value of array of each blind score
-const blind = ["Small Bind", "Big Blind", "Boss Blind"]
+// TODO: so apparently there's a base score for each ante
+// for small blind = 1 * currentbasescore
+// for big blind = 1.5 * currentbasescore
+// for boss blind = 2 * currentbasescore
+// also sometimes boss blind have another unique things for them
+// like making the score bigger than the normal score for the boss
+// or maybe debuffed certain cards
+
+const blind = ["Small Bind", "Big Blind", ""]
 const ante = {
   1: [300, 600, 900],
   2: [300, 600, 900]
 }
-
 export const winCond = (score, boss) => {
   console.log(score, boss)
   return score >= boss
