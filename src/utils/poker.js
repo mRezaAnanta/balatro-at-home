a0a3a1af0221a4abda69c06ee881ae9d8c7108f8
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-01-07 17:49:41 +0700

    feat(pokerhand): four and three of a kind works!

--- src/utils/poker.js
+++ src/utils/poker.js
@@ -1,19 +1,59 @@
 import { $chips } from './handStores.js'
 
+// TODO : determine Straight more dynamically
+// TODO : for some reason the func cannot be called from another func
+const isStraight = (card) => {
+  const straight = {
+    0: '0AJKQ',
+    1: '09JKQ',
+    2: '089JQ',
+    3: '0789J',
+    4: '06789',
+    5: '56789',
+    6: '45678',
+    7: '34567',
+    8: '23456',
+    9: '2345A',
+  }
+  // get the card arr into a sorted str then compare it to the straight obj
+  let str = card.map(x => x[0]).sort().join('')
+  console.log(str)
+  for (let i = 0; i < Object.keys(straight).length; i++) {
+    if (straight[i] === str) {
+      return true
+    }
+  }
+  return false
+}
+
+// isStraight(['AC', 'KC', 'QC', 'JC', '0C'])
+console.log(isStraight(['K', 'Q', 'J', '0', '9']))
+// console.log(isStraight(['KD', 'QD', 'JD', '0D', '9D']))
+// console.log(isStraight(['Q', 'J', '0', '9', '8']))
+// console.log(isStraight(['J', '0', '9', '8', '7']))
+// console.log(isStraight(['0', '9', '8', '7', '6']))
+// console.log(isStraight(['9', '8', '7', '6', '5']))
+// console.log(isStraight(['8', '7', '6', '5', '4']))
+// console.log(isStraight(['7', '6', '5', '4', '3']))
+// console.log(isStraight(['6', '5', '4', '3', '2']))
+// console.log(isStraight(['5', '4', '3', '2', 'A']))
+// console.log(isStraight(['A', 'A', 'Q', 'J', '0']))
+// console.log(isStraight(['K', 'A', 'J', '0', 'Q']))
+// console.log(isStraight(['A', 'A', 'A', '0', 'Q']))
 const pokerHand = {
   'High Card': [5, 1],
   'Pair': [10, 2],
   'Two Pair': [20, 2],
   'Three of a Kind': [30, 3],
-  'Straight': [30, 4],
-  'Flush': [35, 4],
+  'Straight': [30, 4], // done
+  'Flush': [35, 4], // done
   'Full House': [40, 4],
   'Four of a Kind': [60, 7],
-  'Straight Flush': [100, 8],
-  'Royal Flush': [100, 8],
-  'Five of a Kind': [120, 12],
-  'Flush House': [140, 14],
-  'Flush Five': [160, 16],
+  'Straight Flush': [100, 8], // done
+  'Royal Flush': [100, 8], // done
+  'Five of a Kind': [120, 12], // done
+  'Flush House': [140, 14], // done
+  'Flush Five': [160, 16], // done
 }
 
 const determineScore = (card, pokerHand) => {
@@ -24,8 +64,8 @@ const whichPoker = (card) => {
   // card type is array
   // get it's value and determine which poker hand it is based on the rank and suit
   // TODO: seems like i have to count how many card were the same first before i could do anything with it
-  let rank = []
-  let suit = []
+  // let rank = []
+  // let suit = []
   let ranks = {}
   let suits = {}
   let obj = {}
@@ -34,37 +74,7 @@ const whichPoker = (card) => {
   let straight = ['A', 'K', 'Q', 'J', '0', '9', '8', '7', '6', '5', '4', '3', '2', 'A']
   let royal = ['A', 'K', 'Q', 'J', '0']
 
-  // // get rank and suit
-  // card.map(x => {
-  //   rank.push(x[0])
-  //   suit.push(x[1])
-  // })
-  // console.log(rank, suit)
-  //
-  // // ranks obj with how many elements inside of the rank array
-  // // rank array has two Aces and one Jack
-  // // ex: {A: 2, J: 1}
-  // rank.forEach((x) => {
-  //   if (!ranks[x]) {
-  //     ranks[x] = 1
-  //   } else {
-  //     ranks[x]++
-  //   }
-  // })
-  //
-  // // ranks obj with how many elements inside of the rank array
-  // // rank array has two Hearts and one Spade
-  // // ex: {H: 2, S: 1}
-  // suit.forEach((x) => {
-  //   if (!suits[x]) {
-  //     suits[x] = 1
-  //   } else {
-  //     suits[x]++
-  //   }
-  // })
-  //
-  // console.log(ranks, suits)
-
+  // get rank and suit
   card.forEach((x) => {
     if (!obj[x]) {
       obj[x] = 1
@@ -109,6 +119,7 @@ const whichPoker = (card) => {
   if (Object.values(ranks)[0] == 5) {
     console.log('Five of a Kind')
   }
+  //Flush
   if (Object.values(suits)[0] == 5) {
     console.log('Flush')
   }
@@ -135,10 +146,10 @@ const whichPoker = (card) => {
   //   console.log(x, straight.indexOf(x[0]))
   // })
   // console.log(card.map(x => straight.indexOf(x[0])).reduce((acc, cur) => acc + cur, 0))
-  card.map(x => {
-    console.log(x, straight.indexOf(x[0]))
-  })
-  console.log(card.map(x => straight.indexOf(x[0])).sort((a, b) => a - b).join(''))
+  // card.map(x => {
+  //   console.log(x, straight.indexOf(x[0]))
+  // })
+  // console.log(card.map(x => straight.indexOf(x[0])).sort((a, b) => a - b).join(''))
   // let p
   // console.log(card.map(x => straight.indexOf(x[0])).sort().reduce((prev, cur, i) => {
   //   // if x + 1 is not equal to the next x then it's false
@@ -162,8 +173,38 @@ const whichPoker = (card) => {
   if (rFlush == true && Object.values(suits)[0] == 5) {
     console.log('Royal Flush')
   }
-  console.log(rFlush)
+  console.log(`Royal Flush: ${rFlush}`)
 
+  console.log(`straight: ${isStraight(card)}`)
+  // straight Flush
+  if (isStraight(card) == true && Object.values(suits)[0] == 5) {
+    console.log('Straight FLush')
+  }
+  //Four of a Kind
+  // the order is in the sorting ranks 
+  // console.log(Object.values(ranks)[0] == 4)
+  // TODO: make the rank into a str and sort it then compare
+  // console.log(Object.values(ranks))
+  // if (Object.values(ranks)[0] == 4) {
+  //   console.log('Four of a Kind')
+  // }
+  let sorted = []
+  for (let r in ranks) {
+    // TODO: what's r, ranks[r] even means here anyway?
+    sorted.push([r, ranks[r]])
+  }
+  let brah = sorted.sort((a, b) => a[1] - b[1])
+  console.log(brah)
+  console.log(brah[brah.length - 1][1])
+
+  // Four of a Kind
+  if (brah[brah.length - 1][1] == 4) {
+    console.log('Four of a Kind')
+  }
+  // Three of a Kind
+  if (brah[brah.length - 1][1] == 3) {
+    console.log('Three of a Kind')
+  }
 }
 // whichPoker(['AH', 'AS', '2H', 'AH'])
 // whichPoker(['AH', 'AH', 'AH', 'AH', 'AH']) // Flush Five
@@ -172,44 +213,8 @@ const whichPoker = (card) => {
 // whichPoker(['AD', 'KD', 'QD', 'JD', '0D']) // Royal Flush
 // whichPoker(['KD', 'JD', 'QD', '0D', 'AD']) // Royal Flush
 // whichPoker(['AD', '3D', '4D', '5D', '2D']) // Straight
-
-
-// TODO : determine Straight more dynamically
-// TODO : for some reason the func cannot be called from another func
-const isStraight = (card) => {
-  const straight = {
-    0: '0AJKQ',
-    1: '09JKQ',
-    2: '089JQ',
-    3: '0789J',
-    4: '06789',
-    5: '56789',
-    6: '45678',
-    7: '34567',
-    8: '23456',
-    9: '2345A',
-  }
-  let str = card.map(x => x[0]).sort().join('')
-  console.log(str)
-  for (let i = 0; i < Object.keys(straight).length; i++) {
-    if (straight[i] === str) {
-      return true
-    }
-  }
-  return false
-}
-
-// isStraight(['AC', 'KC', 'QC', 'JC', '0C'])
-console.log(isStraight(['K', 'Q', 'J', '0', '9']))
-// console.log(isStraight(['KD', 'QD', 'JD', '0D', '9D']))
-// console.log(isStraight(['Q', 'J', '0', '9', '8']))
-// console.log(isStraight(['J', '0', '9', '8', '7']))
-// console.log(isStraight(['0', '9', '8', '7', '6']))
-// console.log(isStraight(['9', '8', '7', '6', '5']))
-// console.log(isStraight(['8', '7', '6', '5', '4']))
-// console.log(isStraight(['7', '6', '5', '4', '3']))
-// console.log(isStraight(['6', '5', '4', '3', '2']))
-// console.log(isStraight(['5', '4', '3', '2', 'A']))
-// console.log(isStraight(['A', 'A', 'Q', 'J', '0']))
-// console.log(isStraight(['K', 'A', 'J', '0', 'Q']))
-// console.log(isStraight(['A', 'A', 'A', '0', 'Q']))
+// whichPoker(['3D', '3D', '3D', '3D', '2D']) // Four of a Kind
+whichPoker(['2D', '8D', '2D', '8D', '2D']) // Three of a Kind
+// whichPoker(['2D', '8D', '2D', '8D', '3D']) // Two Pair
+// whichPoker(['2D', '8D', '2D', '9D', '0D']) // Pair
+// whichPoker(['2D', '8D', '3D', '9D', '0D']) // High card
