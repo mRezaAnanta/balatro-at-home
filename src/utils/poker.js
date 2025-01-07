32f1e86560c5ab255cb5f82fbf9bde427e5f2ccf
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-01-07 14:45:25 +0700

    feat(pokerhand): straight is implemented

--- src/utils/poker.js
+++ src/utils/poker.js
@@ -116,19 +116,50 @@ const whichPoker = (card) => {
   // Royal Flush
   // get index of each rank element inside card array 
   // if the rank element index is missed by one then it's not straight
-  let rFlush = card.map(x => royal.includes(x[0])).filter(x => x == true)
+  // let rFlush = card.map(x => royal.includes(x[0])).filter(x => x == true)
+  let rFlush = card.every(x => royal.includes(x[0]))
   console.log(straight.indexOf('A'))
   // let aaaa = []
   // for (let i = 0; i < card.length; i++) {
   //   aaaa.push(straight.indexOf(card[i][0]))
   // }
   // console.log(aaaa)
-  console.log(card.map(x => straight.indexOf(x[0])))
-  console.log(card.map(x => straight.indexOf(x[0])).reduce((acc, cur) => acc + cur, 0))
+
+  // Straight
+  console.log(card)
+  // console.log(this.isStraight(card))
+  // if (this.isStraight(card) == true) {
+  //   console.log('Straight')
+  // }
+  // card.map(x => {
+  //   console.log(x, straight.indexOf(x[0]))
+  // })
+  // console.log(card.map(x => straight.indexOf(x[0])).reduce((acc, cur) => acc + cur, 0))
+  card.map(x => {
+    console.log(x, straight.indexOf(x[0]))
+  })
+  console.log(card.map(x => straight.indexOf(x[0])).sort((a, b) => a - b).join(''))
+  // let p
+  // console.log(card.map(x => straight.indexOf(x[0])).sort().reduce((prev, cur, i) => {
+  //   // if x + 1 is not equal to the next x then it's false
+  //   p = prev
+  //   if (p++ == cur) {
+  //     prev + 1
+  //   }
+  //   console.log(prev, cur)
+  //   // } else {
+  //   //   if (prev + 1 == cur) {
+  //   //     prev + 1
+  //   //   } else {
+  //   //     prev = -1
+  //   //   }
+  //   // }
+  // }))
   // for (let i = 0; i < card.length; i++) {
   //   card.indexOf()
   // } 
-  if (rFlush.length == 5 && Object.values(suits)[0] == 5) {
+  // if (rFlush.length == 5 && Object.values(suits)[0] == 5) {
+  if (rFlush == true && Object.values(suits)[0] == 5) {
     console.log('Royal Flush')
   }
   console.log(rFlush)
@@ -138,4 +169,75 @@ const whichPoker = (card) => {
 // whichPoker(['AH', 'AH', 'AH', 'AH', 'AH']) // Flush Five
 // whichPoker(['7D', '4D', '7D', '4D', '7D']) // Flush House
 // whichPoker(['7D', '7S', '7S', '7H', '7C']) // Flush House
-whichPoker(['AD', 'KD', 'QD', 'JD', '0D']) // Royal Flush
+// whichPoker(['AD', 'KD', 'QD', 'JD', '0D']) // Royal Flush
+// whichPoker(['KD', 'JD', 'QD', '0D', 'AD']) // Royal Flush
+// whichPoker(['AD', '3D', '4D', '5D', '2D']) // Straight
+
+
+// let straight = ['A', 'K', 'Q', 'J', '0', '9', '8', '7', '6', '5', '4', '3', '2', 'A']
+// console.log(straight.sort())
+
+// TODO : determine Straight more dynamically
+// TODO : for some reason the func cannot be called from another func
+const isStraight = (card) => {
+  const straight = {
+    // 0: ['A', 'K', 'Q', 'J', '0'],
+    // 1: ['K', 'Q', 'J', '0', '9'],
+    // 2: ['Q', 'J', '0', '9', '8'],
+    // 3: ['J', '0', '9', '8', '7'],
+    // 4: ['0', '9', '8', '7', '6'],
+    // 5: ['9', '8', '7', '6', '5'],
+    // 6: ['8', '7', '6', '5', '4'],
+    // 7: ['7', '6', '5', '4', '3'],
+    // 8: ['6', '5', '4', '3', '2'],
+    // 9: ['5', '4', '3', '2', 'A'],
+    0: '0AJKQ',
+    1: '09JKQ',
+    2: '089JQ',
+    3: '0789J',
+    4: '06789',
+    5: '56789',
+    6: '45678',
+    7: '34567',
+    8: '23456',
+    9: '2345A',
+  }
+  let c = card.map(x => x[0]).sort().join('')
+  // console.log(straight[9].sort().join(''))
+  // console.log(c)
+  // console.log(straight[0].sort().join(''), card.sort().join(''))
+  return straight[0] === c ? true :
+    straight[1].sort().join('') === card.sort().join('') ? true :
+      straight[2].sort().join('') === card.sort().join('') ? true :
+        straight[3].sort().join('') === card.sort().join('') ? true :
+          straight[4].sort().join('') === card.sort().join('') ? true :
+            straight[5].sort().join('') === card.sort().join('') ? true :
+              straight[6].sort().join('') === card.sort().join('') ? true :
+                straight[7].sort().join('') === card.sort().join('') ? true :
+                  straight[8].sort().join('') === card.sort().join('') ? true :
+                    straight[9].sort().join('') === card.sort().join('') ? true : false
+  // return straight[0].sort().join('') === c.sort().join('') ? true :
+  //   straight[1].sort().join('') === c.sort().join('') ? true :
+  //     straight[2].sort().join('') === c.sort().join('') ? true :
+  //       straight[3].sort().join('') === c.sort().join('') ? true :
+  //         straight[4].sort().join('') === c.sort().join('') ? true :
+  //           straight[5].sort().join('') === c.sort().join('') ? true :
+  //             straight[6].sort().join('') === c.sort().join('') ? true :
+  //               straight[7].sort().join('') === c.sort().join('') ? true :
+  //                 straight[8].sort().join('') === c.sort().join('') ? true :
+  //                   straight[9].sort().join('') === c.sort().join('') ? true : false
+}
+
+console.log(isStraight(['AC', 'KC', 'QC', 'JC', '0C']))
+// console.log(isStraight(['K', 'Q', 'J', '0', '9']))
+// console.log(isStraight(['Q', 'J', '0', '9', '8']))
+// console.log(isStraight(['J', '0', '9', '8', '7']))
+// console.log(isStraight(['0', '9', '8', '7', '6']))
+// console.log(isStraight(['9', '8', '7', '6', '5']))
+// console.log(isStraight(['8', '7', '6', '5', '4']))
+// console.log(isStraight(['7', '6', '5', '4', '3']))
+// console.log(isStraight(['6', '5', '4', '3', '2']))
+// console.log(isStraight(['5', '4', '3', '2', 'A']))
+// console.log(isStraight(['A', 'A', 'Q', 'J', '0']))
+// console.log(isStraight(['K', 'A', 'J', '0', 'Q']))
+// console.log(isStraight(['A', 'A', 'A', '0', 'Q']))
