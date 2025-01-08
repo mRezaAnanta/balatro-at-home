867c3657e3d6111be361b33d29520e334eaf7e3b
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-01-08 21:27:39 +0700

    refactor(poker.js): refactoring pokerhand logic

--- src/utils/poker.js
+++ src/utils/poker.js
@@ -25,7 +25,6 @@ const isFiveofaKind = (ranks) => {
   return false
 }
 
-// TODO : determine Straight more dynamically
 const isStraight = (card) => {
   const straight = {
     0: '0AJKQ',
@@ -50,12 +49,11 @@ const isStraight = (card) => {
   return false
 }
 
-// TODO: need to fix
-// return true with flush five
 const isRoyalFlush = (card, suits) => {
-  let royal = ['A', 'K', 'Q', 'J', '0']
-  let rFlush = card.every(x => royal.includes(x[0]))
-  if (rFlush == true && Object.values(suits)[0] == 5) {
+  let royal = '0AJKQ'
+  let str = card.map(x => x[0]).sort().join('')
+  // console.log(royal, str)
+  if (str == royal && Object.values(suits)[0] == 5) {
     // console.log('Royal Flush')
     return true
   }
@@ -80,7 +78,7 @@ const isFourofaKind = (num) => {
 
 const isFullHouse = (ranks) => {
   let r = Object.values(ranks)
-  console.log(r, r[0], r[1])
+  // console.log(r, r[0], r[1])
   if (r[0] == 3 && r[1] == 2 || r[0] == 2 && r[1] == 3) {
     // console.log('Full House')
     return true
@@ -110,6 +108,7 @@ const isTwoPair = (ranks) => {
     // TODO: what's r, ranks[r] even means here anyway?
     sorted.push([r, ranks[r]])
   }
+  // console.log(sorted)
   let brah = sorted.sort((a, b) => a[1] - b[1])
   // console.log(brah)
   // console.log(brah[brah.length - 1][1], brah[brah.length - 2][1])
@@ -156,74 +155,100 @@ const determineScore = (card, pokerHand) => {
 
 }
 
-const whichPoker = (card) => {
+export const whichPoker = (card) => {
   // card type is array
   // get it's value and determine which poker hand it is based on the rank and suit
-  // TODO: seems like i have to count how many card were the same first before i could do anything with it
   let ranks = {}
   let suits = {}
   let obj = {}
 
   // get rank and suit
   card.forEach((x) => {
-    if (!obj[x]) {
-      obj[x] = 1
-    } else {
-      obj[x]++
-    }
+    !obj[x] ? obj[x] = 1 : obj[x]++
+    !ranks[x[0]] ? ranks[x[0]] = 1 : ranks[x[0]]++
+    !suits[x[1]] ? suits[x[1]] = 1 : suits[x[1]]++
   })
-  console.log(card, obj)
-
-  // get the rank and suit
-  card.forEach(x => {
-    if (!ranks[x[0]]) {
-      ranks[x[0]] = 1
-    } else {
-      ranks[x[0]]++
-    }
-  })
-  card.forEach(x => {
-    if (!suits[x[1]]) {
-      suits[x[1]] = 1
-    } else {
-      suits[x[1]]++
-    }
-  })
-  console.log(ranks, suits)
+  // console.log(card, obj, ranks, suits)
 
   // determine how many the ranks is
   let num = 0
   for (const [key, value] of Object.entries(ranks)) {
     if (value > num) num = value
   }
-  console.log(num)
+  // console.log(num)
 
   // which hand it is
-  console.log(`flush five: ${isFlushFive(obj)}`)
-  console.log(`flush house: ${isFlushHouse(obj)}`)
-  console.log(`five of a kind: ${isFiveofaKind(ranks)}`)
-  console.log(`royal flush: ${isRoyalFlush(card, suits)}`)
-  console.log(`straight flush: ${isStraightFlush(card, suits)}`)
-  console.log(`four of a kind: ${isFourofaKind(num)}`)
-  console.log(`full house: ${isFullHouse(ranks)}`)
-  console.log(`flush: ${isFlush(suits)}`)
-  console.log(`straight: ${isStraight(card)}`)
-  console.log(`three of a kind: ${isThreeofaKind(num)}`)
-  console.log(`two pair: ${isTwoPair(ranks)}`)
-  console.log(`pair: ${isPair(num)}`)
-  console.log(`high card: ${isHighCard(num)}`)
+  // console.log(`flush five: ${isFlushFive(obj)}`)
+  // console.log(`flush house: ${isFlushHouse(obj)}`)
+  // console.log(`five of a kind: ${isFiveofaKind(ranks)}`)
+  // console.log(`royal flush: ${isRoyalFlush(card, suits)}`)
+  // console.log(`straight flush: ${isStraightFlush(card, suits)}`)
+  // console.log(`four of a kind: ${isFourofaKind(num)}`)
+  // console.log(`full house: ${isFullHouse(ranks)}`)
+  // console.log(`flush: ${isFlush(suits)}`)
+  // console.log(`straight: ${isStraight(card)}`)
+  // console.log(`three of a kind: ${isThreeofaKind(num)}`)
+  // console.log(`two pair: ${isTwoPair(ranks)}`)
+  // console.log(`pair: ${isPair(num)}`)
+  // console.log(`high card: ${isHighCard(num)}`)
+
+  // TODO: set the order of pokerhand
+  if (isFlushFive(obj)) {
+    return `Flush Five`
+  } else if (isFlushHouse(obj)) {
+    return `Flush House`
+  } else if (isFiveofaKind(ranks)) {
+    return `Five of a Kind`
+  } else if (isRoyalFlush(card, suits)) {
+    return `Royal Flush`
+  } else if (isStraightFlush(card, suits)) {
+    return `Straight Flush`
+  } else if (isFourofaKind(num)) {
+    return `Four of a Kind`
+  } else if (isFullHouse(ranks)) {
+    return `Full House`
+  } else if (isFlush(suits)) {
+    return `Flush`
+  } else if (isStraight(card)) {
+    return `Straight`
+  } else if (isThreeofaKind(num)) {
+    return `Three of a Kind`
+  } else if (isTwoPair(ranks)) {
+    return `Two Pair`
+  } else if (isPair(num)) {
+    return `Pair`
+  } else if (isHighCard(num)) {
+    return `High Card`
+  }
 }
 
+// Test case
 // whichPoker(['AH', 'AH', 'AH', 'AH', 'AH']) // Flush Five
+// console.log(whichPoker(['AH', 'AH', 'AH', 'AH', 'AH'])) // Flush Five
+// console.log(whichPoker(['7D', '4D', '7D', '4D', '7D'])) // Flush House
 // whichPoker(['7D', '4D', '7D', '4D', '7D']) // Flush House
-// whichPoker(['7D', '7S', '7S', '7H', '7C']) // Five of a Kind
+// console.log(whichPoker(['7D', '7S', '7S', '7H', '7C'])) // Five of a Kind
+// console.log(whichPoker(['7D', '7S', '7S', '7H', '7C'])) // Five of a Kind
 // whichPoker(['AD', 'KD', 'QD', 'JD', '0D']) // Royal Flush
-// whichPoker(['KD', 'JD', 'QD', '0D', 'AD']) // Royal Flush
+// console.log(whichPoker(['AD', 'KD', 'QD', 'JD', '0D'])) // Royal Flush
+// whichPoker(['KS', 'JS', 'QS', '0S', 'AS']) // Royal Flush
 // whichPoker(['AD', '3D', '4D', '5D', '2D']) // Straight Flush
+// console.log(whichPoker(['AD', '3D', '4D', '5D', '2D'])) // Straight Flush
 // whichPoker(['3D', '3D', '3D', '3D', '2D']) // Four of a Kind
 // whichPoker(['2D', '8D', '2D', '2D', '8D']) // Full House
-whichPoker(['8D', '8D', '2D', '2D', '8D']) // Full House
+// whichPoker(['8D', '8D', '2D', '2D', '8D']) // Full House
 // whichPoker(['2S', '8D', 'AD', '2D', '2D']) // Three of a Kind
 // whichPoker(['2D', '8D', '2D', '8D', '3D']) // Two Pair
 // whichPoker(['2D', '8D', '2D', '9D', '0D']) // Pair
 // whichPoker(['2D', '8D', '3D', '9D', '0D']) // High card
+// console.log(whichPoker(['3D', '3D', '3D', '3D', '2D'])) // Four of a Kind
+// console.log(whichPoker(['2D', '8D', '2D', '2D', '8D'])) // Full House
+// console.log(whichPoker(['8D', '8D', '2D', '2D', '8D'])) // Full House
+// console.log(whichPoker(['2S', '8D', 'AD', '2D', '2D'])) // Three of a Kind
+// console.log(whichPoker(['2S', '8D', '2D', '8D', '3D'])) // Two Pair
+// console.log(whichPoker(['2S', '8D', '2D', '9D', '0D'])) // Pair
+// console.log(whichPoker(['2S', '8D', '3D', '9D', '0D'])) // High card
+
+// module.exports = {
+//   whichPoker,
+// }
