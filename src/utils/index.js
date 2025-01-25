b472920976058a066c24423c4adac1ae943bbc11
mrezaananta <mRezaAnanta>
2025-01-25 10:05:10 +0700

    feat(utils/index.js): add utils/index.js
    add utils for general stuff, in this case it's for dynamically adding card button when
    drawing/playing/discarding

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const drawing = (drawedCard) => {
+  drawedCard.forEach((element) => {
+    const img = document.createElement('img')
+    const btn = document.createElement('button')
+  
+    // TODO: search how to dynamically import img in astro.js using Image tag from Astro
+    // but atleast just getting the img from the web itself works for now :|
+    img.src = `https://deckofcardsapi.com/static/img/${element}.png`
+    // img.src = `/card/${element}.png`
+    img.classList.add(`w-24`)
+    btn.value = `${element}`
+    btn.classList.add(`card`)
+    // btn.setAttribute('onclick', 'cardSelected(this)')
+  
+    btn.appendChild(img)
+    document.querySelector('div.hand-card-container').appendChild(btn)
+  })
+}
+
+export const cardSelected = (btn) => {
+    alert(btn.value)
+  }
