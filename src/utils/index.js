b472b6255d4dda82a8007ac36d583a9672ee6293
mrezaananta <mRezaAnanta>
2025-02-01 16:11:32 +0700

    feat(utils/index.js): add card-button to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,19 @@
 export const drawing = (drawedCard) => {
   drawedCard.forEach((element) => {
+    const card = document.createElement('card-button')
+    const button = document.createElement('button')
     const img = document.createElement('img')
-    const btn = document.createElement('button')
-  
-    // TODO: search how to dynamically import img in astro.js using Image tag from Astro
-    // but atleast just getting the img from the web itself works for now :|
+
+    button.value = `${element}`
+    console.log(element)
+    card.classList.add(`w-24`)
+  // TODO: search how to dynamically import img in astro.js using Image tag from Astro
+  // but atleast just getting the img from the web itself works for now :|
+  // img.src = `https://deckofcardsapi.com/static/img/${element}.png`
+  // img.src = `/card/${element}.png`
     img.src = `https://deckofcardsapi.com/static/img/${element}.png`
-    // img.src = `/card/${element}.png`
-    img.classList.add(`w-24`)
-    btn.value = `${element}`
-    btn.classList.add(`card`)
-    // FIX: get the onclick attribute to work
-    btn.setAttribute('onclick', 'cardSelected(this)')
-  
-    btn.appendChild(img)
-    document.querySelector('div.hand-card-container').appendChild(btn)
+    card.appendChild(button)
+    button.appendChild(img)
+    document.querySelector('div.hand-card-container').appendChild(card)
   })
-
-}
-
-export const cardSelected = (btn) => {
-  alert(btn.value)
 }
